2501d68a80c1d5ff1d8bcf147b2039045894c142
unknown <mikealaraj@yahoo.com>
2021-07-05 00:21:53 -0500

    first commit

---
+++ posts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from cloudinary.models import CloudinaryField
+
+#class Picture(models.Model):
+  #image = CloudinaryField('image')
+
+class Post(models.Model):
+    class Meta(object):
+        db_table = 'post'
+
+    name = models.CharField(
+        'Name', blank=False, null=False, max_length=14, db_index=True, default='Anonymous'
+    )
+    body = models.CharField(
+        'Body', blank=True, null=True, max_length=140, db_index=True
+    )
+
+    image = CloudinaryField(
+        'image' , blank=True
+    )
+
+    like_count = models.IntegerField(
+        'like_count', default=0, blank=True
+    )
+    created_at = models.DateTimeField(
+        'Created DateTime', blank=True, auto_now_add=True
+    )

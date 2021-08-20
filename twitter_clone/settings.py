7cb366f8d2cd2b586f30130289be107604156a16
unknown <mikealaraj@yahoo.com>
2021-08-20 11:28:57 -0500

    added static root

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -137,6 +137,8 @@ cloudinary.config(
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

3ee9c3928166635ef691906ea3ff0237cb56a091
unknown <mikealaraj@yahoo.com>
2021-08-20 11:31:24 -0500

    added import os

--- twitter_clone/settings.py
+++ twitter_clone/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
